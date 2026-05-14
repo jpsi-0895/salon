79fff2ac8087b136374bb1144bc9101115500b0a
praveensainyyy-creator <praveensainyyy@gmail.com>
2026-05-14 18:47:35 +0530

    ui changes

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,16 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('blogs/home/', views.home),
-    path('blogs/barbers/', views.barber_home),
-    path('blogs/practice/', views.practice_view),
-    path('blogs/learning/',views.learning_view),
-    path('blogs/webpage/',views.webpage,name='webpage'),
-    path('blogs/webpage2/',views.webpage2,name='webpage2'),
-    path('blogs/webpage3/',views.webpage3,name='webpage3'),
-    path('blogs/prs/',views.praveen_hair_salon,name='praveen_hair_salon'),
-    path('blogs/services/', views.services, name='services'),
-    path('blogs/gallery/', views.gallery, name='gallery'),
-    path('blogs/booking/', views.booking, name='booking'),
-    path('blogs/contact/', views.contact, name='contact'),
+    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
+    path('services/', views.services, name='services'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('booking/', views.booking, name='booking'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.salon, name='salon'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -4,26 +4,8 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
 
-# Barber page view
-def barber_home(request):
-    return render(request, 'barbers.html')
-
-def practice_view(request):
-    
-    return render(request, 'practice.html')
-
-def learning_view(request):
-    return render(request,'learning.html')
-def webpage(request):
-    return render(request,'webpage.html')
-
-def webpage2(request):
-    return render(request,'webpage2.html')
-
-def webpage3(request):
-    return render(request,'webpage3.html')
-
-def praveen_hair_salon(request):
+# Salon page view
+def salon(request):
     return render(request,'praveen_hair_salon.html')
 
 def services(request):
